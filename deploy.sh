b96a686468357e3218a43c145965b9a3fe72ec00
thanakiat@netkasystem.com <thanakiat@netkasystem.com>
2023-06-30 15:43:55 +0700

    serverless add api permission

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+aws s3api create-bucket --region ap-southeast-1 --create-bucket-configuration LocationConstraint=ap-southeast-1 --bucket thanakiat-serverless-1
 sam build --region="ap-southeast-1"
-sam deploy --resolve-s3 --region="ap-southeast-1"
+sam deploy --s3-bucket="thanakiat-serverless-2" --region="ap-southeast-1"
